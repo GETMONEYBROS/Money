eed18ef6781783587b59f070a4836ca980abf133
Josiah78 <39107269+Josiah78@users.noreply.github.com>
2018-05-26 11:17:03 -0500

    video work 5/26

---
+++ index2.js
@@ -0,0 +1,65 @@
+ // Comment 1400
+ console.log('Hello World');
+
+
+ let name = 'Playboi Carti';
+ console.log(name);
+
+ // Cant be reserved keyword
+ // meaningful
+ // cant start w #
+ // no space or hyphen
+ // case sensitive
+
+ let firstName; = 'Playboi';
+ let lastName = 'Carti';
+
+
+
+ const interestRate = 0.3;
+ interestRate = 1;
+ console.log(interestrate);
+
+
+ let name = 'Playboi Carti'; // String Literal
+ let age = 20; // Number Literal
+ let isApproved = false; // Boolean Literal
+ let firstName = undefined;
+ let selectedColor = null;
+
+
+ let person = {
+     name: 'Playboi Carti',
+     age: 20
+ };
+
+ // Dot Notation
+ person.name = 'east';
+
+ // Bracket Notation
+ LET SELECTION = 'name';
+ person['selection'] = 'ATL';
+ console.log(person.name);
+
+
+
+ let selectedColors = ['red', 'blue'];
+ selectedColors[2] = 1;
+ console.log(selecedColors.length);
+
+
+// Performing a ttask
+ function greet(name, lastName) {
+     console.log('Hello ' + name + ' ' + lastName);
+ }
+
+// calculating a value
+function square(number) {
+    return numbner * number;
+}
+
+let number = ;
+console.log(square(2));
+
+
+ greet('east', 'ATL'); 
