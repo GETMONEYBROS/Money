ba03fbb7091c926dd59fe9156474cd7bfe25033e
Andre Paradise <portland_state@yahoo.com>
2018-05-26 23:35:22 -0700

    made 'js' folder and we will start saving all of our javascript files in the folder. ONLY javascript files will go here.

---
+++ js/dom.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//install ESlint first then watch this video below. These two steps will fix the ES6 error issues for variable warnings.
+// i watched this video to fix the 'const' underline error issue for ES6 https://www.youtube.com/watch?v=cMrDePs86Uo
+
+const name = 'Playboi Carti'; 
+
+const infoText = ' i fixed the issue with the const key word. I will teach show you.' 
+
+var x = document.getElementById("title");
+x.innerHTML = name;
+
+document.getElementById("info").innerHTML = infoText;
+
+const mySon1 = 'Josiah';
+console.log(mySon1);
