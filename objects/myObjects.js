5e88a522909195192144571605cacd4ca5f8df5d
Josiah78 <39107269+Josiah78@users.noreply.github.com>
2018-06-04 22:34:21 -0500

    images testing/nudy

---
+++ objects/myObjects.js
@@ -0,0 +1,30 @@
+let rappers = {
+    currency: 7000000,
+    drug: 'lean',
+    carType: 'foreign cars'
+};
+
+let house = {
+    rooms: 'bedrooms',
+    emenitities: 'swimming pool',
+    petIntrance: 'doggy door'
+};
+
+document.write(rappers.drug);   
+console.log('young nudty is worth: ' + rappers.currency);
+console.log('money');
+console.log('lean');
+console.log('foreign cars');
+console.log('bedrooms');
+console.log('swimming pool');
+console.log('doggy door');
+
+const  songs = ['EA','Goyard','YUNGXANHOE'];
+
+console.log(songs);
+
+
+
+
+
+
