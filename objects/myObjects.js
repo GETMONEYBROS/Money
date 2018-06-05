c66977f632e57b5e0095d7875831cac901554081
Josiah78 <39107269+Josiah78@users.noreply.github.com>
2018-06-05 00:27:23 -0500

    arrays for loop new Object keyword without using Object literal testing

--- objects/myObjects.js
+++ objects/myObjects.js
@@ -1,3 +1,4 @@
+//this is an object literal
 let rappers = {
     currency: 7000000,
     drug: 'lean',
@@ -23,8 +24,70 @@ const  songs = ['EA','Goyard','YUNGXANHOE'];
 
 console.log(songs);
 
+for(let i=0; i<songs.length; i++) {
+    console.log(   songs[i]     );
+}
+
+console.log(songs.length);
 
 
+for(let i=0;  i<=5;  i++) {
+    console.log(i);
+}
+
+// new keyword & Object constructor
+const student = new Object();
+ 
+student.grade = 12;
+student.gradePointAverage = 3.7;
+student.classes = ["English", "Algebra", "Chemistry"];
+student.getClasses = function() {
+  return this.classes;
+};
+ 
+ console.log(student.grade);
+ console.log(student.gradePointAverage);
+ console.log(student.classes);
+ console.log(student.getClasses);
+ console.log(student.getClasses());
 
 
-
+ //below we will work on this after I learn function
+  
+// constructor function 
+function Car(color, brand, year) {
+    this.color = color;
+    this.brand = brand;
+    this.year = year;
+  }
+   
+  Car.prototype.getColor = function() {
+    return this.color;
+  };
+   
+  const carlysCar = new Car('blue', 'ferarri', 2015);
+  const jimsCar = new Car('red', 'tesla', 2014);
+   
+  console.log(carlysCar.getColor());
+  console.log(jimsCar.getColor());
+  
+  
+  
+  //something to know...
+  const user1 = {
+    name: 'Jordan',
+    age: 28,
+  };
+   
+  const user2 = {
+    name: 'Jordan',
+    age: 28,
+  };
+   
+  console.log(user1 == user2); //return false because objects DO NOT compare in value only reference
+  console.log(user1 === user2); //return false because objects DO NOT compare in value only reference
+  //now to make this true you must delete user2 and  write your reference as: const user2 = user1;
+  
+  //the other way to get a value and not reference is to use JSON.stringify() to convert the object to a string then it can be compared by value
+  //because strings are primative data types and do not contain properties.
+  // console.log(JSON.stringify(user1) === JSON.stringify(user2);
---
+++ var.js
@@ -0,0 +1,16 @@
+
+function start() {
+    for {let i = 0}; i < 5; i++)
+     if (true) {
+         var color = 'red';
+     }
+console.log(color);
+}
+
+
+// var => function-scoped
+// ES6 (ES2015): let, const => block-scoped
+start();
+
+var color = 'red';
+let age = 30;
