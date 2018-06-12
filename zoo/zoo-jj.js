bcbe78e3709348aaa7525b7fe611162c8cfe5d81
Josiah78 <39107269+Josiah78@users.noreply.github.com>
2018-06-11 23:10:42 -0500

    zoo updated.

--- zoo/zoo-jj.js
+++ zoo/zoo-jj.js
@@ -1,29 +1,25 @@
-
-
 var animals = {
     hot:"camels",
     cold:"polar bears",
     water:"sharks"
 };
 
-
-let coworkers = ['Kenna,'Josiah','Malik'];
+const coworkers = ['Kenna','Josiah','Malik'];
 
 var coworkersSalaries = {
-    best:"1,000,000,000",
-    optimal:"1,000,000",
-    worst:"100,000"
+    best:1000000000,
+    optimal:1000000,
+    worst:100000
 };
 
-coworkersSalaries(coworkersSalariesKenna, coworkersSalaries){
+function coworkersSalaries(coworkersSalariesKenna, coworkersSalaries){
     return    '$' + (40 *   coworkersSalaries)  + ' hr  is how much' +   coworkersSalariesKenna + ' makes per week.';
     }
 
-workersSalary(coworkersSalariesJosiah, coworkersSalaries){
-        return    '$' + (40 *   coworkersSalaries)  + ' hr  is how much' +   coworkersSalariesJosiah + ' makes per week.';
-        }
+function workersSalary(coworkersSalariesJosiah, coworkersSalaries){
+    return    '$' + (40 *   coworkersSalaries)  + ' hr  is how much' +   coworkersSalariesJosiah + ' makes per week.';
+    }
 
-bigMoney(coworkersSalariesMalik, coWorkersSalaries){
-            return    '$' + (40 *   coworkersSalaries)  + ' hr  is how much' +   coworkersSalariesMalik + ' makes per week.';
-            }
-
+function bigMoney(coworkersSalariesMalik, coWorkersSalaries){
+    return    '$' + (40 *   coworkersSalaries)  + ' hr  is how much' +   coworkersSalariesMalik + ' makes per week.';
+    }
