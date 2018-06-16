07295215a68d2d11db9a4f1feac8b7201d23bc18
aparadise <portland_state@yahoo.com>
2018-06-15 21:52:26 -0700

    OOP learning for Object Oriented Programming

---
+++ dad/dad-oop.js
@@ -0,0 +1,71 @@
+// OOP = object oriented programing
+
+//example 1 using 'this' keyword in method/function in objbect literal
+var foo = {
+    one: 1,
+    two: 2,
+    three: 'three',
+    four:function(){ //when a function is inside of objects key:value its a "method" 
+        return document.getElementById('oop2').innerHTML = 29;
+    },
+    five: function(){
+        return document.getElementById('oop3').innerHTML = this.two;
+    }
+}
+
+foo.four(); //this is how you return the value of the method/function in your objects key:value
+
+foo.five(); //this is how you return the value of the method/function in your objects key:value
+
+
+//example 2 passing arguments/parameters
+
+function car(a,b,c){
+    //alert( a+b+c);  
+    return document.getElementById('oop1').innerHTML = a + b + c;
+    console.log( a + b + c);
+}
+
+car(1,2,"blah");
+
+//example 3 using the key word 'this' for object oriented programming OOP
+//this is called a 'constructor function' also known as 'classes'
+//and classes and contructors need to have 'capital letter' so people know what it is.
+//your class/constructor is known as 'blue print'
+function House(size, color, type, city, state){
+    this.size = size,
+    this.color = color,
+    this.type = type,
+    this.city = city,
+    this.state = state
+    console.log( this.size + " " +  this.color + " " +  this.type + " " + ' i will by it for sale 5k in' +  this.city + " " + this.state);
+}
+
+//house(2, 'red', 'condo' );
+//notice the keyword 'new' this is how you make and 'instance' of the class/constructor function
+const friscoHills = new House(5, 'blue', 'trailer park', 'frisco', 'tx');
+const beverlyHills = new House(15, 'green', 'mantion', 'beverly hills', 'ca');
+const portlandNW= new House(152, 'white', 'apartment', 'portland', 'or');
+
+
+
+//how to make a self-invoking your function on load.
+function jj(){
+    return document.getElementById('oop4').innerHTML = 'hello world invoke me';
+}
+
+jj(); //self-invoking
+
+
+//another way to self-invoking
+//all self-invoking function means it will invoke right way
+(function aj(){
+    return document.getElementById('oop5').innerHTML = 'hello world andre';
+})(); //notice the '()' invoke parentheses
+
+
+//this is called anonymous function when the function is in a variable!
+const myNumber = function(){
+    return document.getElementById('oop6').innerHTML = 'i am anonymous function';
+}
+myNumber(); //this is how you call your anonymous function
