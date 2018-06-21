1e47b69bb680eec153319bc5ad310b9f59ff7ad4
Josiah78 <39107269+Josiah78@users.noreply.github.com>
2018-06-21 15:44:01 -0500

    Pets Template Literals Project DONE. FRFR THIS TIME.

--- pets/js/pets.js
+++ pets/js/pets.js
@@ -1,23 +1,34 @@
 const petsData = [
 
 {
-name: "purrsLoud",
+name: "Playboi Carti",
 species: "cat",
 favFoods: ["wet food", "dry food", "<strong>any</strong> food"],
-birthYear: 2018
+birthYear: 2018,
+photo: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&h=350"
 },
 
 {
-name: "barksAlot",
+name: "Famous Dex",
 species: "dog",
-birthYear: 2017
+birthYear: 2017,
+photo: "https://www.rover.com/blog/wp-content/uploads/2017/06/7683806832_59688557b2_o-960x540.jpg"
 },
 
 {
-name: "meowsAlot",
+name: "Rich The Kid",
 species: "cat",
 favFoods: ["tuna", "catnip", "celery"],
-birthYear: 2008
+birthYear: 2008,
+photo: "http://rs568.pbsrc.com/albums/ss123/minzayminzay/Decorated%20images/WhiteCatFace.gif?w=280&h=210&fit=crop"
+},
+
+{
+name: "LIL UZI VERT",
+species: "cat",
+favFoods: ["nothing it starves", "poop", "piss"],
+birthYear: 2008,
+photo: "https://craigturic.files.wordpress.com/2016/08/catredeyes1.jpg?w=630"
 }
 ];
 
