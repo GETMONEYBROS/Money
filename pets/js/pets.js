9f8f1f569f3cbcf03e11de9c5696c43dfacf4625
Josiah78 <39107269+Josiah78@users.noreply.github.com>
2018-06-21 16:28:22 -0500

    josiahs template literals

--- pets/js/pets.js
+++ pets/js/pets.js
@@ -1,35 +1,35 @@
 const petsData = [
+    {
+        name: "Playboi Carti",
+        species: "cat",
+        favFoods: ["wet food", "dry food", "<strong>any</strong> food"],
+        birthYear: 2018,
+        photo: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&h=350"
+    },
 
-{
-name: "Playboi Carti",
-species: "cat",
-favFoods: ["wet food", "dry food", "<strong>any</strong> food"],
-birthYear: 2018,
-photo: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&h=350"
-},
+    {
+        name: "Famous Dex",
+        species: "dog",
+        birthYear: 2017,
+        photo: "https://www.rover.com/blog/wp-content/uploads/2017/06/7683806832_59688557b2_o-960x540.jpg"
+    },
 
-{
-name: "Famous Dex",
-species: "dog",
-birthYear: 2017,
-photo: "https://www.rover.com/blog/wp-content/uploads/2017/06/7683806832_59688557b2_o-960x540.jpg"
-},
+    {
+        name: "Rich The Kid",
+        species: "cat",
+        favFoods: ["tuna", "catnip", "celery"],
+        birthYear: 2008,
+        photo: "http://rs568.pbsrc.com/albums/ss123/minzayminzay/Decorated%20images/WhiteCatFace.gif?w=280&h=210&fit=crop"
+    },
 
-{
-name: "Rich The Kid",
-species: "cat",
-favFoods: ["tuna", "catnip", "celery"],
-birthYear: 2008,
-photo: "http://rs568.pbsrc.com/albums/ss123/minzayminzay/Decorated%20images/WhiteCatFace.gif?w=280&h=210&fit=crop"
-},
-
-{
-name: "LIL UZI VERT",
-species: "cat",
-favFoods: ["nothing it starves", "poop", "piss"],
-birthYear: 2008,
-photo: "https://craigturic.files.wordpress.com/2016/08/catredeyes1.jpg?w=630"
-}
+    {
+        name: "LIL UZI VERT",
+        species: "cat",
+        favFoods: ["nothing it starves", "poop", "piss"],
+        birthYear: 2008,
+        photo: "https://craigturic.files.wordpress.com/2016/08/catredeyes1.jpg?w=630",
+        state: "Texas"
+    }
 ];
 
 function age(birthYear) {
@@ -58,10 +58,11 @@ function foods(foods) {
 function petTemplate(pet) {
 return `
 <div class="animal">
-<img class="pet-photo" src="${pet.photo}">
-<h2 class="pet-name">${pet.name} <span>(${pet.species})</span></h2>
-<p><strong>age:</strong> ${age(pet.birthYear)}</p>
-${pet.favFoods ? foods(pet.favFoods) : ""}
+    <img class="pet-photo" src="${pet.photo}">
+    <h2 class="pet-name">${pet.name} <span>(${pet.species})</span></h2>
+    <p><strong>age:</strong> ${age(pet.birthYear)}</p>
+    ${pet.favFoods ? foods(pet.favFoods) : ""}
+    <p>${pet.state}</p>
 </div>
  `
 }
