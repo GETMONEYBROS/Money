de8e2a8c6ee56d3c800c9e0d4c0a94ea28128a06
Josiah78 <39107269+Josiah78@users.noreply.github.com>
2019-05-03 15:42:55 -0500

    typos, new code practing.

---
+++ attemptToCode.js
@@ -0,0 +1,36 @@
+var a = "Kenna West";
+document.getElementById("Wifey").innerHTML = a;
+
+document.write("I Love Kenna West");
+
+function kenna(){
+    console.log("I Love Kenna");
+};
+kenna();
+
+var west = function(){
+    console.log("West");
+};
+west();
+
+(function oregon(){
+    console.log("Portland");
+})();
+
+const tigard = () => {
+    console.log("Tigard");
+};
+tigard();
+
+var object1 = {
+    clothes: "designer",
+    drink: "lean",
+    smoke: "gas"
+};
+alert(object1.smoke);
+
+Object.keys(object1);
+console.log(Object.keys(object1));
+
+const c = ["kenna", "josiah"];
+console.log(c);
